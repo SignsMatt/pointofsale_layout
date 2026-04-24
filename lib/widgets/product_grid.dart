445f9144f503146aa1f917b5eafdc8cc4db02190
Matt Alexander <85204672+SignsMatt@users.noreply.github.com>
2026-04-24 17:54:22 -0600

    Add fade effect to ProductGrid

--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pointofsale_layout/models/product.dart';
+import 'package:pointofsale_layout/theme/app_colors.dart';
 import 'package:pointofsale_layout/widgets/product_card.dart';
 
 class ProductGrid extends StatelessWidget {
@@ -13,17 +14,29 @@ class ProductGrid extends StatelessWidget {
       builder: (context, constraints) {
         final crossAxisCount = _crossAxisCountForWidth(constraints.maxWidth);
 
-        return GridView.builder(
-          itemCount: products.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: crossAxisCount,
-            mainAxisSpacing: 14,
-            crossAxisSpacing: 14,
-            childAspectRatio: 1.9,
-          ),
-          itemBuilder: (context, index) {
-            return ProductCard(product: products[index]);
-          },
+        return Stack(
+          children: [
+            GridView.builder(
+              padding: const EdgeInsets.only(bottom: 56),
+              itemCount: products.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: crossAxisCount,
+                mainAxisSpacing: 14,
+                crossAxisSpacing: 14,
+                childAspectRatio: 1.9,
+              ),
+              itemBuilder: (context, index) {
+                return ProductCard(product: products[index]);
+              },
+            ),
+            const Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              height: 60,
+              child: IgnorePointer(child: _ProductGridFade()),
+            ),
+          ],
         );
       },
     );
@@ -34,5 +47,22 @@ class ProductGrid extends StatelessWidget {
     if (width >= 720) return 3;
     if (width >= 450) return 2;
     return 1;
+  }
+}
+
+class _ProductGridFade extends StatelessWidget {
+  const _ProductGridFade();
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [AppColors.white.withValues(alpha: 0), AppColors.white],
+        ),
+      ),
+    );
   }
 }
